534eef3fe1e9e87f794fa2f1c4b37097bbbb2363
Abel Fisseha <51496964+abelTooLIT@users.noreply.github.com>
2021-07-05 01:34:26 +0300

    pep8

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+    Represents a state class  that inherits from BaseModel
+'''
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    '''
+        Represent a state class
+    '''
+    name = ""
