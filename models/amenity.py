2e38a379f9b542e5b29ad925897e78bc2321bfd7
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-07-10 15:36:37 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -7,6 +7,6 @@ from models.base_model import BaseModel
 
 class Amenity (BaseModel):
     '''
-        Represents Class Amenity with public class attribute
+        Represents Class Amenity with public class attributes
     '''
     name = ""
