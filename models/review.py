f215f219bf090787ff13f97dbb774d4cd842dd95
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-07-10 15:39:25 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -7,7 +7,7 @@ from models.base_model import BaseModel
 
 class Review (BaseModel):
     '''
-        Represents Class Review  with public class attribute
+        Represents Class Review  with public class attributes
     '''
     place_id = ""
     user_id = ""
